5f66098875fc28705ee2468dc03497fbd30c1f00
unknown <bao.hn184046@sis.hust.edu.vn>
2021-12-07 10:30:01 +0700

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
+  resources :books
+  root 'static_pages#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
